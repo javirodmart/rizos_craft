07772078e4cbae3b8e3d707d33829eafc906ab9d
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2023-03-22 01:18:21 -0500

    updated production.rb

--- bin/risos-build.sh
+++ bin/risos-build.sh
@@ -3,9 +3,9 @@
  set -o errexit
 
  #front end build
- rm -rf public
- npm install --prefix client && npm run build --prefix client 
- cp -a client/build/. public/
+#  rm -rf public
+#  npm install --prefix client && npm run build --prefix client 
+#  cp -a client/build/. public/
 
 #backend build commands
 bundle install
