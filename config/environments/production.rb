fc67e281616f9845a133494fe4860dce2f2e6f9a
Javier Rodriguez <javi.rodriguez.martinz@gmail.com>
2023-03-22 01:07:15 -0500

    updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rizos_Crafts::Application.configure do
+RizosCrafts::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
